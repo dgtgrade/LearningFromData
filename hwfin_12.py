53820a15ed438a8aa2f301f41e2bd0c416d74d1a
dgtgrade <dgtgrade@gmail.com>
2016-12-01 20:42:26 +0900

    final 12완료

---
+++ hwfin_12.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from quadprog import solve_qp
+
+X = np.array([[1, 0], [0, 1], [0, -1], [-1, 0], [0, 2], [0, -2], [-2, 0]],
+             dtype=np.double)
+Y = np.array([-1, -1, -1, 1, 1, 1, 1],
+             dtype=np.double)
+
+assert len(X) == len(Y)
+
+N = len(Y)
+
+
+def MK(X_):
+    return (1 + X_.dot(X_.T))**2
+
+G = Y.dot(Y.T) * MK(X)
+a = np.ones(N, dtype=np.double)
+C = np.array(np.c_[Y, np.eye(N, N)], dtype=np.double)
+b = np.zeros(1 + N, dtype=np.double)
+
+# to suppress positive definite error
+G += np.eye(N, N) * 1e-14
+
+alphas, _, _, _, _, _ = solve_qp(G, a, C, b, meq=1)
+
+print(alphas)
+print(np.sum(alphas > 1e-3))
