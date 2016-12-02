b89fe3c983d79955203c6902466ace7537cd3168
dgtgrade <dgtgrade@gmail.com>
2016-12-02 19:09:24 +0900

    G 값 관련 버그 수정

--- hwfin_12.py
+++ hwfin_12.py
@@ -4,7 +4,7 @@ from quadprog import solve_qp
 
 X = np.array([[1, 0], [0, 1], [0, -1], [-1, 0], [0, 2], [0, -2], [-2, 0]],
              dtype=np.double)
-Y = np.array([-1, -1, -1, 1, 1, 1, 1],
+Y = np.array([[-1], [-1], [-1], [1], [1], [1], [1]],
              dtype=np.double)
 
 assert len(X) == len(Y)
@@ -15,13 +15,13 @@ N = len(Y)
 def MK(X_):
     return (1 + X_.dot(X_.T))**2
 
-G = Y.dot(Y.T) * MK(X)
+G = np.outer(Y, Y) * MK(X)
 a = np.ones(N, dtype=np.double)
 C = np.array(np.c_[Y, np.eye(N, N)], dtype=np.double)
 b = np.zeros(1 + N, dtype=np.double)
 
 # to suppress positive definite error
-G += np.eye(N, N) * 1e-14
+G += np.eye(N, N) * 1e-13
 
 alphas, _, _, _, _, _ = solve_qp(G, a, C, b, meq=1)
 
